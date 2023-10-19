f77dcef7a54ad830a0bc4d597a283cb3af3ecf5c
Bankole Ojo <ojobankole@gmail.com>
2023-10-19 13:52:30 +0100

    have a top bar

--- nv349/main.go
+++ nv349/main.go
@@ -14,6 +14,10 @@ import (
 
 const (
 	fps = 10
+
+	AddImgBtn = 1
+	AddVidBtn = 2
+	OpenWDBtn = 3
 )
 
 var objCoords map[int]g143.RectSpecs
@@ -63,6 +67,64 @@ func allDraws(window *glfw.Window) {
 	if err != nil {
 		panic(err)
 	}
+
+	// add image button
+	addImgStr := "Add Image"
+	addImgStrWidth, addImgStrHeight := ggCtx.MeasureString(addImgStr)
+	addImgBtnWidth := addImgStrWidth + 80
+	addImgBtnHeight := addImgStrHeight + 30
+	ggCtx.SetHexColor("#B75F5F")
+	ggCtx.DrawRoundedRectangle(10, 10, addImgBtnWidth, addImgBtnHeight, addImgBtnHeight/2)
+	ggCtx.Fill()
+
+	addImgBtnRS := g143.RectSpecs{Width: int(addImgBtnWidth), Height: int(addImgBtnHeight),
+		OriginX: 10, OriginY: 10}
+	objCoords[AddImgBtn] = addImgBtnRS
+
+	ggCtx.SetHexColor("#fff")
+	ggCtx.DrawString(addImgStr, 30, 10+addImgStrHeight+15)
+
+	ggCtx.SetHexColor("#633232")
+	ggCtx.DrawCircle(10+addImgBtnWidth-40, 10+addImgBtnHeight/2, 10)
+	ggCtx.Fill()
+
+	// Add Video Button
+	addVidStr := "Add Video"
+	addVidStrWidth, addVidStrHeight := ggCtx.MeasureString(addVidStr)
+	addVidBtnWidth := addVidStrWidth + 80
+	addVidBtnHeight := addVidStrHeight + 30
+	ggCtx.SetHexColor("#81577F")
+	addVidBtnOriginX := float64(addImgBtnRS.Width+addImgBtnRS.OriginX) + 10 // gutter
+	ggCtx.DrawRoundedRectangle(addVidBtnOriginX, 10, addVidBtnWidth, addVidBtnHeight, addVidBtnHeight/2)
+	ggCtx.Fill()
+
+	addVidBtnRS := g143.RectSpecs{Width: int(addVidBtnWidth), Height: int(addVidBtnHeight),
+		OriginX: int(addVidBtnOriginX), OriginY: 10}
+	objCoords[AddVidBtn] = addVidBtnRS
+
+	ggCtx.SetHexColor("#fff")
+	ggCtx.DrawString(addVidStr, 30+float64(addVidBtnRS.OriginX), 10+addImgStrHeight+15)
+
+	ggCtx.SetHexColor("#633260")
+	ggCtx.DrawCircle(float64(addVidBtnRS.OriginX)+addVidBtnWidth-30, 10+addVidBtnHeight/2, 10)
+	ggCtx.Fill()
+
+	// Open Working Directory button
+	owdStr := "Open Working Directory"
+	owdStrWidth, owdStrHeight := ggCtx.MeasureString(owdStr)
+	openWDBtnWidth := owdStrWidth + 30
+	openWDBtnHeight := owdStrHeight + 30
+	ggCtx.SetHexColor("#56845A")
+	openWDBtnOriginX := float64(addVidBtnRS.OriginX+addVidBtnRS.Width) + 40
+	ggCtx.DrawRoundedRectangle(openWDBtnOriginX, 10, openWDBtnWidth, openWDBtnHeight, openWDBtnHeight/2)
+	ggCtx.Fill()
+
+	openWDBtnRS := g143.RectSpecs{Width: int(openWDBtnWidth), Height: int(openWDBtnHeight),
+		OriginX: int(openWDBtnOriginX), OriginY: 10}
+	objCoords[OpenWDBtn] = openWDBtnRS
+
+	ggCtx.SetHexColor("#fff")
+	ggCtx.DrawString(owdStr, 15+float64(openWDBtnRS.OriginX), 10+owdStrHeight+15)
 
 	// send the frame to glfw window
 	windowRS := g143.RectSpecs{Width: wWidth, Height: wHeight, OriginX: 0, OriginY: 0}
